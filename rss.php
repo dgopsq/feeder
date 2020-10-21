61c47f0f78b814ab57a39a9914894fecac12d64c
Diego Pasquali <5963683+dgopsq@users.noreply.github.com>
2020-10-21 18:09:16 +0200

    Update rss.php

--- rss.php
+++ rss.php
@@ -61,8 +61,11 @@ echo '<?xml version="1.0" encoding="UTF-8"?>'."\n";
 	foreach ($products AS $product)
 	{
 		$image = Image::getImages((int)($cookie->id_lang), $product['id_product']);
+		$price = html_entity_decode(Tools::displayPrice(Product::getPriceStatic($product['id_product']), $currency), ENT_COMPAT, 'UTF-8');
+		$stripped_description = strip_tags($product['description_short']);
+			
 		echo "\t\t<item>\n";
-		echo "\t\t\t<title><![CDATA[".$product['name']." - ".html_entity_decode(Tools::displayPrice(Product::getPriceStatic($product['id_product']), $currency), ENT_COMPAT, 'UTF-8')." ]]></title>\n";
+		echo "\t\t\t<title><![CDATA[".$product['name']."]]></title>\n";
 		echo "\t\t\t<description>";
 		$cdata = true;
 		if (is_array($image) AND sizeof($image))
@@ -73,8 +76,9 @@ echo '<?xml version="1.0" encoding="UTF-8"?>'."\n";
 		}
 		if ($cdata)
 			echo "<![CDATA[";
-		echo $product['description_short']."]]></description>\n";
+		echo $stripped_description."]]></description>\n";
 
+		echo "\t\t\t<price><![CDATA[".$price."]]></price>\n";
 		echo "\t\t\t<link><![CDATA[".str_replace('&amp;', '&', htmlspecialchars($link->getproductLink($product['id_product'], $product['link_rewrite'], Category::getLinkRewrite((int)($product['id_category_default']), $cookie->id_lang)))).$affiliate."]]></link>\n";
 		echo "\t\t</item>\n";
 	}
